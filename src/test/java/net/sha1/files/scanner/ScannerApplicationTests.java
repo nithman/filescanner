7e2986fbfcec0bdd9f3d60e01e9722353b25be40
Norman Nithman <norman@nithman.org>
2020-09-21 06:51:57 -0500

    added tests

---
+++ src/test/java/net/sha1/files/scanner/ScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sha1.files.scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
